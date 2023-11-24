29d8dea1d382652d88915ac78dd9b9179ed3a5f6
dongha-yoon <cs.donghayoon@gmail.com>
2023-11-24 04:13:56 -0500

    fio

--- fio.sh
+++ fio.sh
@@ -5,19 +5,18 @@ do_lmbench() {
 
 	for i in {1..5}
 	do
-		echo "start lmbench $1 tr_$i"
-		numactl -C 8-15 bash -c "../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 4096m rd >> lmbench_res/fio_${fioname}_rd.res"
-		numactl -C 8-15 bash -c "../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 4096m wr >> lmbench_res/fio_${fioname}_wr.res"
-		numactl -C 8-15 bash -c "../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 4096m rdwr >> lmbench_res/fio_${fioname}_rdwr.res"
+		echo "start lmbench $1 tr $i"
+		numactl -C 8-15 ../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 4096m rd 2>> lmbench_res/fio_${fioname}_rd.res
+		numactl -C 8-15 ../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 4096m wr 2>> lmbench_res/fio_${fioname}_wr.res
+		numactl -C 8-15 ../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 4096m rdwr 2>> lmbench_res/fio_${fioname}_rdwr.res
 	done
-
 	echo "lmbench $1 done"
 }
 
 do_fio() {
 	fioname=$1
 	echo "numactl -C 4-7 fio pmem_${fioname}.fio -output=fio_res/pmem_${fioname}.output"
-	numactl -C 4-7 fio pmem_${fioname}.fio -output=fio_res/pmem_${fioname}.output
+	numactl -C 4-7 fio pmem_${fioname}.fio -output=fio_res/pmem_${fioname}.output >> log
 	
 	echo "$fioname done" >> lmbench_res/fio_${fioname}_rd.res
 	echo "$fioname done" >> lmbench_res/fio_${fioname}_wr.res
@@ -33,6 +32,7 @@ rm fio_res/*
 for wl in $fioworkloads
 do
 	do_fio $wl &
+	sleep 30s
 	do_lmbench $wl
 done
 

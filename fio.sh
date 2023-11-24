d5dd86b5c417eb50b1050670ec9ab2ca74ed5cc3
dongha-yoon <cs.donghayoon@gmail.com>
2023-11-24 03:47:40 -0500

    fio

--- fio.sh
+++ fio.sh
@@ -5,10 +5,13 @@ do_lmbench() {
 
 	for i in {1..5}
 	do
+		echo "start lmbench $1 tr_$i"
 		numactl -C 8-15 ../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 4096m rd >> lmbench_res/fio_${fioname}_rd.res
 		numactl -C 8-15 ../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 4096m wr >> lmbench_res/fio_${fioname}_wr.res
 		numactl -C 8-15 ../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 4096m rdwr >> lmbench_res/fio_${fioname}_rdwr.res
 	done
+
+	echo "lmbench $1 done"
 }
 
 do_fio() {

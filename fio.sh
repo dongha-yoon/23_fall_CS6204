6e5e4c8e9cdfa759ed3a23329071229e2d7bf422
dongha-yoon <cs.donghayoon@gmail.com>
2023-11-24 03:59:07 -0500

    fio

--- fio.sh
+++ fio.sh
@@ -6,9 +6,9 @@ do_lmbench() {
 	for i in {1..5}
 	do
 		echo "start lmbench $1 tr_$i"
-		numactl -C 8-15 ../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 4096m rd >> lmbench_res/fio_${fioname}_rd.res
-		numactl -C 8-15 ../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 4096m wr >> lmbench_res/fio_${fioname}_wr.res
-		numactl -C 8-15 ../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 4096m rdwr >> lmbench_res/fio_${fioname}_rdwr.res
+		numactl -C 8-15 bash -c "../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 4096m rd >> lmbench_res/fio_${fioname}_rd.res"
+		numactl -C 8-15 bash -c "../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 4096m wr >> lmbench_res/fio_${fioname}_wr.res"
+		numactl -C 8-15 bash -c "../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 4096m rdwr >> lmbench_res/fio_${fioname}_rdwr.res"
 	done
 
 	echo "lmbench $1 done"

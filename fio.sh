24ece5ff5c64ed2fd949cba1c0fa8b24500e9c5e
dongha-yoon <cs.donghayoon@gmail.com>
2023-11-25 03:26:46 -0500

    fio

--- fio.sh
+++ fio.sh
@@ -6,9 +6,9 @@ do_lmbench() {
 	for i in {1..15}
 	do
 		echo "start lmbench $1 tr $i"
-		numactl -C 8-15 ../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 16m rd 2>> lmbench_res/fio_${fioname}_rd.res
-		numactl -C 8-15 ../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 16m wr 2>> lmbench_res/fio_${fioname}_wr.res
-		numactl -C 8-15 ../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 16m rdwr 2>> lmbench_res/fio_${fioname}_rdwr.res
+		numactl -C 8-15 ../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 8m rd 2>> lmbench_res/fio_${fioname}_rd.res
+		numactl -C 8-15 ../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 8m wr 2>> lmbench_res/fio_${fioname}_wr.res
+		numactl -C 8-15 ../lmbench/bin/x86_64-linux-gnu/bw_mem -P 8 8m rdwr 2>> lmbench_res/fio_${fioname}_rdwr.res
 	done
 	echo "lmbench $1 done"
 }
@@ -29,7 +29,7 @@ export PMEM_IS_PMEM_FORCE=1
 rm lmbench_res/*
 rm fio_res/*
 
-for wl in $fioworkloads
+for wl in "${fioworkloads[@]}"
 do
 	do_fio $wl 
 	do_fio $wl &
